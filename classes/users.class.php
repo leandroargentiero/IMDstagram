637d0b0b88512a42b480e15551008bea2cfed6c2
leandroargentiero <argentieroleo@gmail.com>
2016-04-04 18:58:10 +0200

    Added class users and getters/setters

---
+++ classes/users.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leandro
+ * Date: 04/04/16
+ * Time: 18:53
+ */
+class Users{
+    private $m_sEmail;
+    private $m_sFullname;
+    private $m_sUsername;
+    private $m_sPassword;
+
+    public function __set($p_sProperty, $p_vValue)
+    {
+
+    }
+
+    public function __get($p_sProperty)
+    {
+
+    }
+
+}
