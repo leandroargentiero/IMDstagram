e996a23bba7be8740fa54a07fd0a8adb7cfb7e02
leandroargentiero <argentieroleo@gmail.com>
2016-04-04 17:14:56 +0200

    finished lay out sign up

--- signup.php
+++ signup.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-	
+    <div class="container">
+        <div class="header">
+            <img class="logo" src="images/logo.png" alt="logo instagram">
+            <p class="introduction">Sign up to see photos and videos from your friends.</p>
+            <a class="btnLoginFacebook" href="#">Log in with Facebook</a>
+            <div class="separator">
+                <div class="separator-line"></div>
+                <p class="separator-or">OR</p>
+            </div>
+        </div>
+        <div class="signup-form">
+            <form action="">
+                <input type="email" name="email" id="email" placeholder="Email">
+                <input type="text" name="fullname" id="fullname" placeholder="Full Name">
+                <input type="text" name="username" id="username" placeholder="Username">
+                <input type="password" name="password" id="password" placeholder="Password">
+				
+               	<input type="submit" name="btnSignUp" id="btnSignup" value="Sign up" >
+            </form>
+        </div>
+        <footer>
+            <p>By signin up, you agree to our <br> <span>Terms &amp; Privacy Policy</span></p>
+        </footer>
+    </div>
 </body>
+
 </html>
