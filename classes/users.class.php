ebb073bc9b7a6fefd42719527c29e0163fe97f2d
leandroargentiero <argentieroleo@gmail.com>
2016-04-04 19:15:24 +0200

    updated getters and setters

--- classes/users.class.php
+++ classes/users.class.php
@@ -13,12 +13,38 @@ class Users{
 
     public function __set($p_sProperty, $p_vValue)
     {
-
+        switch($p_sProperty){
+            case "Email":
+                $this->m_sEmail = $p_vValue;
+                break;
+            case "fullname":
+                $this->m_sFullname = $p_vValue;
+                break;
+            case "Username":
+                $this->m_sUsername = $p_vValue;
+                break;
+            case "Password":
+                $this->m_sPassword = $p_vValue;
+                break;
+        }
     }
 
     public function __get($p_sProperty)
     {
-
+        switch($p_sProperty){
+            case "Email":
+                return $this->m_sEmail;
+                break;
+            case "Fullname":
+                return $this->m_sFullname;
+                break;
+            case "Username":
+                return $this->m_sUsername;
+                break;
+            case "Password":
+                return $this->m_sPassword;
+                break;
+        }
     }
 
 }
