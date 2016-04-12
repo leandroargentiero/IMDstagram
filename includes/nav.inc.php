010b06aec2f41c02770d2184da0a3ce779a5ba0d
leandroargentiero <argentieroleo@gmail.com>
2016-04-12 16:44:18 +0200

    show current username in nav

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,12 +1,11 @@
-
-    <div class="headerProfile">
-        <div class="headerContent">
-            <img src="images/logo.png" alt="logo">
-            <div class="searchBar">
-                <form>
-                    <span><input type="text"  placeholder="Search"></span>
-                </form>
-            </div>
-            <h2>kennymn</h2>
+<div class="headerProfile">
+    <div class="headerContent">
+        <img src="images/logo.png" alt="logo">
+        <div class="searchBar">
+            <form>
+                <span><input type="text"  placeholder="Search"></span>
+            </form>
         </div>
+        <h2><?php echo $_SESSION['user']; ?></h2>
     </div>
+</div>
