7e3d6a4d655fb0c898e4735c4b4d0ec4ba3d624f
Diederik <diederik127@hotmail.com>
2016-04-14 13:32:35 +0200

    file upload - local storage

--- upload.php
+++ upload.php
@@ -1,5 +1,26 @@
 <?php
     include_once('includes/no-session.inc.php');
+
+    //this file contains the code to process the upload of files
+    if ($_FILES["file"]["error"] > 0){
+        //for error messages: see http://php.net/manual/en/features.fileupload.errors.php
+        switch($_FILES["file"]["error"]){
+            case 1:
+                $error = "U mag maximaal 2MB opladen.";
+                break;
+            default:
+                $error = "Sorry, uw upload kon niet worden verwerkt.";
+        }
+        echo $error . "<br />";
+    } 
+    else
+    {
+        echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+        echo "Type: " . $_FILES["file"]["type"] . "<br />";
+        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+        echo "Stored in: " . $_FILES["file"]["tmp_name"];
+    } 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +39,10 @@
         </div>
         <h2>Upload an image</h2>
         <div class="form">
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+                <input type="file" name="file" id="fileUpload" />
+                <br />
+                <input type="submit" name="submit" value="Upload Now!" />
 
                 <?php
                 if( isset($error) ) {
