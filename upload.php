8df4b559f619a97c49c8c94595e6d969278e2719
Diederik <diederik127@hotmail.com>
2016-04-14 15:26:00 +0200

    upload wordt verplaatst van temp naar /files
    TODO: upload bijhouden in mySQL

--- upload.php
+++ upload.php
@@ -1,58 +1,71 @@
 <?php
     include_once('includes/no-session.inc.php');
 
-    //this file contains the code to process the upload of files
-    if ($_FILES["file"]["error"] > 0){
-        //for error messages: see http://php.net/manual/en/features.fileupload.errors.php
-        switch($_FILES["file"]["error"]){
-            case 1:
-                $error = "U mag maximaal 2MB opladen.";
-                break;
-            default:
-                $error = "Sorry, uw upload kon niet worden verwerkt.";
-        }
-        echo $error . "<br />";
-    } 
-    else
-    {
-        echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-        echo "Type: " . $_FILES["file"]["type"] . "<br />";
-        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-        echo "Stored in: " . $_FILES["file"]["tmp_name"];
-    } 
-
+    
+if ($_FILES["file"]["error"] > 0)
+{
+//for error messages: see http://php.net/manual/en/features.fileupload.errors.php
+switch($_FILES["file"]["error"])
+ {
+ case 1:
+ $error = "U mag maximaal 2MB opladen.";
+ break;
+ default:
+ $error = "Sorry, uw upload kon niet worden verwerkt.";
+ }
+echo $error . "<br />";
+} 
+else
+{
+echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+echo "Type: " . $_FILES["file"]["type"] . "<br />";
+echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+echo "Stored in: " . $_FILES["file"]["tmp_name"];
+    $filename = $_FILES["file"]["tmp_name"];
+ $finfo = new finfo(FILEINFO_MIME_TYPE);
+ $fileinfo = $finfo->file($filename);
+    $newfilename = "files/" . $_FILES["file"]["name"];
+    $_SESSION['image'] = $newfilename;
+    if(move_uploaded_file($_FILES["file"]["tmp_name"], $newfilename))
+ {
+        echo "<br><a href='showImage.php'>Check image</a>";
+        echo $_SESSION['image'];
+ }
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Imdstragram</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+    
+    <!DOCTYPE html>
+    <html lang="en">
 
-<body>
-    <div class="container">
-        <div class="header">
-            <img class="logo" src="images/logo.png" alt="logo instagram">
-        </div>
-        <h2>Upload an image</h2>
-        <div class="form">
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-                <input type="file" name="file" id="fileUpload" />
-                <br />
-                <input type="submit" name="submit" value="Upload Now!" />
+    <head>
+        <meta charset="UTF-8">
+        <title>Imdstragram</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
 
-                <?php
+    <body>
+        <div class="container">
+            <div class="header">
+                <img class="logo" src="images/logo.png" alt="logo instagram">
+            </div>
+            <h2>Upload an image</h2>
+            <div class="form">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+                    <input type="file" name="file" id="fileUpload" />
+                    <br />
+                    <input type="submit" name="submit" value="Upload Now!" />
+
+                    <?php
                 if( isset($error) ) {
                     echo "<p class='error'>$error</p>";
                 }
 
                 ?>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 
-</html>
+    </html>
