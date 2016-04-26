f748fd09d5dc37a5a2c127b954e9a249ca583add
leandroargentiero <argentieroleo@gmail.com>
2016-04-26 16:43:35 +0200

    postDetail will show details about post/picture

--- postDetail.php
+++ postDetail.php
@@ -1,23 +1,14 @@
 <?php
     session_start();
     include_once ("includes/nav.inc.php");
+    include_once ("classes/postDetail.class.php");
 
     if(isset($_GET['imageID'])){
-        $imageID = $_GET['imageID'];
-        $result = array();
+        $post = new postDetail();
+        $post->imageID = $_GET['imageID'];
+        $image = $post->getImage();
+        $avatar = $post->getAvatar();
 
-        $conn = new PDO('mysql:host=localhost; dbname=imdstagram', 'root', 'root');
-        $statement = $conn->prepare("
-                            SELECT *
-                            FROM posts
-                            WHERE imageID = :imageID");
-        $statement->bindValue(':imageID', $imageID );
-        $statement->execute();
-
-        if($statement->rowCount() == 1){
-            $result = $statement->fetch(PDO::FETCH_ASSOC);
-
-        }
     }
 
 ?><!doctype html>
@@ -48,14 +39,74 @@
     .innerRight{
         width: 400px;
         height: 400px;
-        background-color: red;
+        background-color: white;
     }
+    .innerRightContainer{
+        padding: 2em;
+    }
+    .innerRightHeader{
+        height: 50px;
+        border-bottom: 1px solid #dddbd9;
+    }
+    .innerRightSecondHeader{
+        height: 50px;
+        border-bottom: 1px solid #dddbd9;
+    }
+    .innerRight .commentFeed{
+        height: 200px;
+        border-bottom: 1px solid #dddbd9;
+
+    }
+    .avatar{
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+        float: left;
+        display: inline-block;
+        margin: -10px 0 0 0;
+    }
+    .name{
+        font-family: 'instaBold', 'sans-serif';
+        font-size: .9em;
+        display: inline-block;
+        margin: 9px 0 0 20px;
+        color: #30618B;
+    }
+    .likes{
+        display: inline-block;
+        float: left;
+        margin: 15px 0 0 0;
+    }
+    .timestamp{
+        display: inline-block;
+        margin: 15px 0 0 230px;
+    }
+
 
 </style>
 <body>
     <div class="postDetail">
-        <div class="innerLeft"><img src="<?php echo $result['fileLocation']; ?>" alt=""></div>
-        <div class="innerRight"></div>
+        <div class="innerLeft"><img src="<?php echo $image['fileLocation']; ?>" alt=""></div>
+        <div class="innerRight">
+            <div class="innerRightContainer">
+                <div class="innerRightHeader">
+                    <img class="avatar" src="<?php echo $avatar['avatar']; ?>" alt="">
+                    <p class="name">Test123</p>
+                    </ul>
+                </div>
+                <div class="innerRightSecondHeader">
+                    <p class="likes">146 Likes</p>
+                    <p class="timestamp">16H</p>
+                </div>
+                <div class="commentFeed"></div>
+                <div class="innerRightFooter">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
