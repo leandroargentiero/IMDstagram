7899c55a29c5a4e18c26a54148593e21b13e542c
leandroargentiero <argentieroleo@gmail.com>
2016-05-02 17:20:37 +0200

    changed layout of search bar

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -3,7 +3,7 @@
         <a href="index.php"><img src="images/logo.png" alt="logo"></a>
         <div class="searchBar">
             <form name="formSearch" method="get" action="search.php">
-                <span><input type="text" name="txtSearch" placeholder="Search" required></span>
+                <span><input id="txtSearch" type="text" name="txtSearch" placeholder="Search" required></span>
             </form>
         </div>
         <a href="profile.php"><h2><?php echo $_SESSION['user']; ?></h2></a>
