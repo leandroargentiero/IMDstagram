21e94456a9cbe879942877055cb12af8c6330752
leandroargentiero <argentieroleo@gmail.com>
2016-05-09 15:04:57 +0200

    fixed typo

--- includes/like.inc.php
+++ includes/like.inc.php
@@ -23,10 +23,7 @@ if(!empty($likeReceiverID)){
     $statement->bindValue(':likeSenderID', $likeSenderID);
     $statement->bindValue(':likeReceiverID', $likeReceiverID);
     $statement->execute();
-    if($statement->execute()){
-        console.log("send");
-        echo "liked";
-    };
 }
+
 
 
