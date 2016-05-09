870eda638fb19a0e592c6020838aae62e4fb541e
leandroargentiero <argentieroleo@gmail.com>
2016-05-09 11:38:25 +0200

    Added click event with ajax call

--- js/scripts.js
+++ js/scripts.js
@@ -2,46 +2,44 @@ $(document).ready(function(){
     init();
     $('.btnEditProfile').click(function(){
         console.log('click');
-   window.location.href='editProfile.php';
-})
+        window.location.href='editProfile.php';
+    });
     function init(){
-    $('.btnFollow').click(function(){
-        $.ajax({
-                        type : 'POST',
-                        url  : 'includes/follow.inc.php',
-                        data : $(this).serialize(),
-                        success : function(data)
-                        {
-                            console.log(data);
-                            console.log("succes!")
-                            $(".btnFollow").attr("class", "btnUnfollow");
-                            $(".btnUnfollow").html("Volgend");
-                            init();
-                        }
-                    });
-                    return false;
-    });
-    
-    $('.btnUnfollow').click(function(){
-        $.ajax({
-                        type : 'POST',
-                        url  : 'includes/unfollow.inc.php',
-                        data : $(this).serialize(),
-                        success : function(data)
-                        {
-                            console.log(data);
-                            console.log("succes!")
-                            $(".btnUnfollow").attr("class", "btnFollow");
-                            $(".btnFollow").html("Volgen");
-                            init();
-                        }
-                    });
-                    return false;
-    });
-        
-    
-};
-$('.btnLoadMore').click(function(){
+        $('.btnFollow').click(function(){
+            $.ajax({
+                            type : 'POST',
+                            url  : 'includes/follow.inc.php',
+                            data : $(this).serialize(),
+                            success : function(data)
+                            {
+                                console.log(data);
+                                console.log("succes!");
+                                $(".btnFollow").attr("class", "btnUnfollow");
+                                $(".btnUnfollow").html("Volgend");
+                                init();
+                            }
+            });
+            return false;
+        });
+
+        $('.btnUnfollow').click(function(){
+            $.ajax({
+                            type : 'POST',
+                            url  : 'includes/unfollow.inc.php',
+                            data : $(this).serialize(),
+                            success : function(data)
+                            {
+                                console.log(data);
+                                console.log("succes!");
+                                $(".btnUnfollow").attr("class", "btnFollow");
+                                $(".btnFollow").html("Volgen");
+                                init();
+                            }
+            });
+            return false;
+        });
+    }
+    $('.btnLoadMore').click(function(){
     for(i=0; i<20;i++){
         $.ajax({
                         
@@ -60,5 +58,18 @@ $('.btnLoadMore').click(function(){
                     });}
                     return false;
     
+    });
+
+    $('.like-btn').click(function(){
+        $.ajax({
+            type: 'POST',
+            url: 'includes/like.inc.php',
+            data: $(this).serialize(),
+            success: function(data)
+            {
+                console.log(data);
+            }
+        });
+        return false;
     });
 });
