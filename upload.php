8a5edf54b5dade8baf793e7070ad669f0ed5b772
leandroargentiero <argentieroleo@gmail.com>
2016-05-14 17:54:55 +0200

    Deleted input type field

--- upload.php
+++ upload.php
@@ -80,8 +80,7 @@ else
                         <option value="willow">willow</option>
                         <option value="xpro2">xpro2</option>
                     </select>
-
-                    <input type="file" name="file" id="fileUpload" />
+                    
                     <input type="hidden" name="location" id="location" value="">
 
                     <label for="description">Description:</label>
