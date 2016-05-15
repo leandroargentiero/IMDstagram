5c084ba2cc4759c1cb9b91e129f4bed8a727b683
kennymn <kenny_malangombe@hotmail.com>
2016-05-15 16:17:14 +0200

    update

--- includes/comment_insert.php
+++ includes/comment_insert.php
@@ -5,7 +5,15 @@
 
     //userID
     //commentID
+    $commentUserID = $_SESSION['userID'];
+    $commentImageID = $_POST['imageID'];
 
+    $statement = $conn->prepare("INSERT INTO comments(commentUserID, commentImageID, commentText)
+                                  VALUES(:commentUserID, :commentImageID, :commentInsert())");
+    $statement->bindValue(':commentUserID',$commentUserID);
+    $statement->bindValue(':commentImageID', $commentImageID);
+    $statement->bindValue(':commentText', commentInsert());
+    $statement->execute();
 
 
 
