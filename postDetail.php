71fae24a80f2d9adc4235715f62400e246c478e4
leandroargentiero <argentieroleo@gmail.com>
2016-05-16 18:21:03 +0200

    Placing comments WORKS

--- postDetail.php
+++ postDetail.php
@@ -1,8 +1,8 @@
 <?php
     include_once ("includes/no-session.inc.php");
     include_once ("includes/nav.inc.php");
-    include_once ("includes/comment.inc.php");
     include_once ("classes/postDetail.class.php");
+    include_once ("classes/users.class.php");
     $visible = "";
     $_SESSION['imageID'] = $_GET['imageID'];
 
@@ -15,6 +15,7 @@
         $likeCount = $post->getLikes($_GET['imageID']);
         $userID = $post->getUserID($_GET['imageID']);
         $description = $post->getDescription($_GET['imageID']);
+        $comments = $post->getComments($_GET['imageID']);
 
         $likeCheck = $post->likeCheck($_GET['imageID']);
         if($likeCheck)
@@ -33,7 +34,7 @@
         $visible = "visible";
     }
 
-
+    $i = "";
 
 ?><!doctype html>
 <html lang="en">
@@ -42,7 +43,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cssgram-cssgram.netdna-ssl.com/cssgram.min.css">
+    <link rel="stylesheet" href="css/cssgram.min.css">
 </head>
 
 <body>
@@ -57,7 +58,7 @@
                 <div class="innerRightHeader">
 
                     <a href="profile.php?userID=<?php echo $userID['imageUserID']; ?>"><img class="avatarPostDetail" src="<?php echo $avatar['avatar']; ?>" alt=""></a>
-                    <a href="profile.php?userID=<?php echo $userID['imageUserID']; ?>"><p class="name"><?php echo $username['username']; ?></p></a>
+                    <a href="profile.php?userID=<?php echo $userID['imageUserID']; ?>"><p class="name<?php echo $i ?>"><?php echo $username['username']; ?></p></a>
                     </ul>
                 </div>
                 <div class="innerRightSecondHeader">
@@ -79,20 +80,32 @@
                     <p class="timestamp"><?php echo $postTime ?></p>
                 </div>
                 <div class="commentFeed">
-                    <ul class="commentFeed-holder">
-                        <?php $comments = array("a", "b", "c", "d"); ?>
-                            <?php foreach( $comments as $key => $comment): ?>
-                            <li class="commentFeed-items" id="_1"><span class="comment-username"></span><span class="comment-text"><?php echo $commentText['commentText']; ?></span></li>
-                            <?php endforeach; ?>
-
+                    <ul class="commentsList">
+                        <li><a href="profile.php?userID=<?php echo $comment['commentUserID']; ?>">
+                                <?php echo $username['username']; ?>
+                            </a>
+                            <span class="comment-text"><?php echo $description['description']; ?></span>
+                        </li>
+                        <?php foreach( $comments as $comment): ?>
+                            <li>
+                                <a href="profile.php?userID=<?php echo $comment['commentUserID']; ?>">
+                                    <?php
+                                    $user = new Users();
+                                    $user->getProfile($comment['commentUserID']);
+                                    $username = $user->Username;
+                                    echo $username;
+                                    ?></a>
+                                <span class="comment-text"><?php echo $comment['commentText'] ?></span>
+                            </li>
+                        <?php endforeach; ?>
                     </ul>
                 </div>
                 <div class="innerRightFooter">
                     <form id="commentForm" class="innerRightFooterForm" action="" method="post">
                         <img id="heart" class="likeHeart <?php echo $class ?>" src="<?php echo $source ?>" alt="like"
                         value="<?php echo $_GET['imageID']; ?>">
-                        <input id="commentField" type="text" name="commentField" placeholder="Add a comment...">
-                        <input id="comment-btn-submit" type="submit" style="position: absolute; left: -9999px"/>
+                        <input class="commentField commentField<?php echo $i ?>" type="text" name="commentField" placeholder="Add a comment...">
+                        <input class="comment-btn-submit" type="submit" style="position: absolute; left: -9999px" value="<?php echo $i ?>"/>
                     </form>
                     
                     <span class="glyphicon glyphicon-trash" id="<?php echo $visible; ?>" aria-hidden="true" title="Verwijder je foto"></span>
@@ -101,8 +114,8 @@
             </div>
         </div>
     </div>
-    <input type="hidden" id="userID" value="1">
-    <input type="hidden" id="userName" value="<?php $_SESSION['userID'] ?>">
+    <input type="hidden" class="imageID<?php echo $i; ?>" value="<?php echo $_GET['imageID'].$i; ?>">
+    <input type="hidden" class="userID<?php echo $i; ?>" value="<?php echo $userID['imageUserID'].$i; ?>">
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="js/scripts.js">
